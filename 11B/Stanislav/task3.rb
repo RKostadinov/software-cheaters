a8f63037cd960334ef2554af25345fc001ab29b1
gigobyte <gigobest2@gmail.com>
2014-11-27 01:22:33 +0200

    Update task3.rb

--- 11B/Stanislav/task3.rb
+++ 11B/Stanislav/task3.rb
@@ -1,5 +1,13 @@
-#sushtoto kato task2.rb, no
-#if word == "software"
-#se zamenq s
-#if word.length == 5
-#i gg
+=begin
+Develop a program named FirstName_LastName_ClassNumber_<%=@task_number%>.rb
+You are given an argument for a folder with files.
+Count how many of the words have <%=@length%> letters.
+Sort the result by number of times found in <%=@in_what_order%> order.
+Produce a result in <%=@format%> format named result.<%=@format%>.
+=end
+
+sushtoto kato task2.rb, no
+  if word == "software"
+se zamenq s
+  if word.length == 5
+i gg
