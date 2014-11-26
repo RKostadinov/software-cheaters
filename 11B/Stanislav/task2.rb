32abc865e0b854b4f497c19edb8c7916e6718c6c
gigobyte <gigobest2@gmail.com>
2014-11-26 23:02:08 +0200

    Update task2.rb

--- 11B/Stanislav/task2.rb
+++ 11B/Stanislav/task2.rb
@@ -1,9 +1,16 @@
 =begin
 Develop a program named FirstName_LastName_ClassNumber_<%=@task_number%>.rb
+
+
+
 You are given an argument for a folder with files.
+
 Count how many times the word "software" occurs in each of the files.
+
 Sort the result by times the word is found in DESC.
+
 Produce a result in CSV format named result.csv
+
 =end
 
 require 'csv'
@@ -16,8 +23,7 @@ Dir.glob("#{ARGV[0]}/**/*.*").each do |raw_file|
 	file = File.open("#{raw_file}")
 	file_read = file.readlines
 	file_read.each do |line|
-		words = line.split(" ")
-		words.each do |word|
+		words = line.split(" ").each do |word|
 			if word == "software"
 				i += 1
 				count_hash[filename] = i
